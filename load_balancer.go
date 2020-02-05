9f51a7769e3207b3647078a05b573c1d7d92b320
jscode017 <jasonhack518@gmail.com>
2020-02-05 20:17:42 +0800

    add heartbeat service

--- load_balancer.go
+++ load_balancer.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"bufio"
-	"context"
+	//"context"
 	"io"
 	"log"
-	"math"
 	"net"
 	"sync"
+	"time"
 )
 
 type LoadBalancer struct {
-	ServerIps []string
-	Address   string
-	Seed      uint32
+	ServerIps         []string
+	Address           string
+	Seed              uint32
+	HeartBeatDuration time.Duration
+	ServerTimeOut     time.Duration
 	sync.RWMutex
 }
 
-func NewLoadBalancer(serverIPs []string) *LoadBalancer {
+func NewLoadBalancer(serverIPs []string, heartBeatDuration int, serverTimeOut int) *LoadBalancer {
 	//serversIp := []string{"localhost:8081", "localhost:8082"}
 	return &LoadBalancer{
-		ServersIp: serverIPs,
-		Address:   "localhost:8085",
+		ServerIps:         serverIPs,
+		Address:           "localhost:8085",
+		HeartBeatDuration: time.Duration(heartBeatDuration) * time.Second,
+		ServerTimeOut:     time.Duration(serverTimeOut) * time.Second,
 	}
 }
 
@@ -30,23 +34,44 @@ func (lb *LoadBalancer) Run() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("accept a connection")
-		go lb.ReverseProxy(conn)
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Println("accept a connection")
+			go lb.ReverseProxy(conn)
 
+		}
+	}()
+	ticker := time.NewTicker(lb.HeartBeatDuration)
+	defer ticker.Stop()
+	select {
+	case <-ticker.C:
+		serverIpIndex := 0
+		for serverIpIndex < len(lb.ServerIps) {
+			log.Println("sending heart beat to: ", lb.ServerIps[serverIpIndex])
+			err = lb.SendHeartBeat(serverIpIndex)
+			if err != nil {
+				log.Println("error from server ", lb.ServerIps[serverIpIndex], err)
+				lb.Lock()
+				lb.ServerIps = append(lb.ServerIps[:serverIpIndex], lb.ServerIps[serverIpIndex+1:]...)
+				lb.Unlock()
+			} else {
+				log.Println(lb.ServerIps[serverIpIndex], " is healthy")
+				serverIpIndex++
+			}
+		}
 	}
 }
 
 func (lb *LoadBalancer) ReverseProxy(conn net.Conn) {
 	defer conn.Close()
 	lb.Lock()
-	curSeed := atomic.LoadUint32(&lb.Seed) //though is a read op, but still want to get an unique seed
-	serverIPIndex := lb.Seed % len(lb.ServerIps)
-	serverIP := lb.ServerIps[serverIPIndex]
+	serverIpIndex := lb.Seed % uint32(len(lb.ServerIps)) //though is a read op, but still want to get an unique seed, so use lock instead of rlock
+	serverIP := lb.ServerIps[serverIpIndex]
 	lb.Seed++ //do not need to handle overflow, it would simply become 0 if overflow
 	lb.Unlock()
 	dst, err := net.Dial("tcp", serverIP)
@@ -56,8 +81,20 @@ func (lb *LoadBalancer) ReverseProxy(conn net.Conn) {
 
 	lbReader := bufio.NewReader(conn)
 	log.Println("directing")
-	ctx := context.WithCancel(context.Background())
+	//ctx,cancel := context.WithCancel(context.Background())
 	go io.Copy(conn, dst)
 	io.Copy(dst, lbReader)
+
+}
+
+func (lb *LoadBalancer) SendHeartBeat(index int) error {
+	serverIP := lb.ServerIps[index]
+	conn, err := net.DialTimeout("tcp", serverIP, lb.ServerTimeOut)
+	defer conn.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
