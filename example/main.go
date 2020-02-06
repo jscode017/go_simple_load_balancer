e5b894a2f1839e172d23900d4339e86c4ae8a42e
jscode017 <jasonhack518@gmail.com>
2020-02-06 18:44:53 +0800

    change package name

---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+// a simple server for testing
+import (
+	"github.com/gin-gonic/gin"
+	loadbalancer "github.com/jscode017/go_simple_load_balancer"
+)
+
+func main() {
+	lb := loadbalancer.NewLoadBalancer([]string{"localhost:8081", "localhost:8082"}, 10, 2)
+	go lb.Run()
+	r1 := gin.Default()
+	r2 := gin.Default()
+
+	r1.GET("/", Hello1)
+	r2.GET("/", Hello2)
+	go r1.Run(":8081")
+	r2.Run(":8082")
+}
+
+func Hello1(c *gin.Context) {
+	c.JSON(200, gin.H{"result": true, "hello": "1"})
+	return
+}
+
+func Hello2(c *gin.Context) {
+	c.JSON(200, gin.H{"result": true, "hello": "2"})
+	return
+}
